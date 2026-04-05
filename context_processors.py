144b4bd0a3bbcb3f01c7dba159e825a131af403c
Mohansravanth <mohansravanthvarma123@gmail.com>
2026-04-05 21:40:59 +0530

    Delete context_processors.py

--- context_processors.py
+++
@@ -1,8 +0,0 @@
-from .models import DEPARTMENT_CHOICES, POST_TYPE_CHOICES
-
-def categories_processor(request):
-    """Provide departments and post types to all templates"""
-    return {
-        'departments': DEPARTMENT_CHOICES,
-        'post_types': POST_TYPE_CHOICES,
-    }
